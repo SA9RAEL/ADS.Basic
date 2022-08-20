8ca22498eaea47c158d3368620eda6ec90747739
SA9RAEL <sa9rael@yandex.ru>
2022-08-20 13:51:55 +0500

    Added and tested dynamic array.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,21 @@
-fun main(){
+package ADS.Basic.src.main.kotlin
+
+fun main() {
+    val list = DynamicArray()
+    var n = 15
+    repeat(n) {
+        list.add(n--)
+    }
+
+    println(list)
+    list.remove(13)
+    list.remove(12)
+    list.remove(11)
+    list.remove(10)
+    list.remove(9)
+    list.remove(8)
+    list.remove(7)
+    list.remove(6)
+    println(list)
 
 }
